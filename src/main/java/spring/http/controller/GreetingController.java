050c75cf4ead3f8dd45a0764aab6bdc747411ac3
R1no933 <dbaskakov93@yandex.ru>
2023-10-08 12:09:47 +0300

    WEB Starter. Parameters, header and cookies example

--- src/main/java/spring/http/controller/GreetingController.java
+++ src/main/java/spring/http/controller/GreetingController.java
@@ -1,14 +1,21 @@
 package spring.http.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView helloWorld(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView helloWorld(ModelAndView modelAndView, HttpServletRequest request,
+                                   @RequestParam("age") Integer age,
+                                   @RequestHeader("accept") String accept,
+                                   @CookieValue("JSESSIONID") String jsessionId,
+                                   @PathVariable("id") Integer id) {
+
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
